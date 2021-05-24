59928d2f4e6b0f2440f53d51cc7df621b567570a
Ansari <63479371+lecyberzilla@users.noreply.github.com>
2021-05-24 14:56:24 +0500

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(void)
+{
+int pid,status;
+pid=fork();
+if(pid>0)
+{
+printf(*In Parent Process \nVUID: MC200204513\nM. Saqlain Nasir: \n*);
+
+printf("Existing the Parent Process");
+wait(0);
+exit(1);
+}
+else if(pid==0){
+int n= 3;
+int i;
+printf("In Child Process\nmultiplication table of 3 \n*);
+for(i=1; 1<=10; i++)
+{
+printf("%d * %d= %d \n" ,n,i,i*n);
+}
+}
+}
